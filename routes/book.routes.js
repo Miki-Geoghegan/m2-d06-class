f0e57f35952eb3a90b0f094ca72c1c6bdc62c718
Miki Geoghegan <michaelageoghegan@hotmail.co.uk>
2021-06-28 16:12:31 +0200

    newest commit

--- routes/book.routes.js
+++ routes/book.routes.js
@@ -22,23 +22,79 @@ router.get('/', (req, res) => { // with app.js we would use app.get ('/books', e
 // this next method answers to the /books/add URL - we want this to happen when someone is in /books/add 
 // we just write /add because we are already in books (from app.js, as above)
 router.post('/add', (req, res) => {
- const title = req.body.title
+
+ // if you know the name of the properties, you can comment out all of the const variable creations and add:
+ const {title, description, author, rating} = req.body // this is sugar, you can asign default values and rename properties
+ // this is called DESTRUCTURING and is another way of doing the below - it is the same
+
+/*  const title = req.body.title
  const description = req.body.description
  const author = req.body.author
- const rating = req.body.rating
-// next need to create a book:
- Book.create({title, description, author, rating})
+ const rating = req.body.rating */
+
+
+ // next need to create a book:
+ Book.create({title, description, author, rating}) // this is saying mongoose, create a book with this object inside it
  .then(createdBook => console.log(createdBook))
+ .catch (err => console.log(err)) // after creating the book, it is important to redirect the user back to the books i.e.:
+ res.redirect('/books')
+ // you could also send them back to an edit form, where they can change the value if they made a mistake
 })
 // by deciding that the user will send a post form, we access the body and from here we can get the field we need
+
+router.get('/add', (req, res) => { // we are referring to the books.hbs file, we use GET because we have used a link and can only use GET with links
+  res.render('books-add')
+})
+// think of the route and the name ('books-add'), then create a file with that name for it to relate to
+
+
+
+
+
+// to update a book you need to know:
+// 1. all of the different ids in the object that you are updating
+router.post('/edit/:id', (req, res) => {
+  const id = req.params.id
+  const {title, description, author, rating} = req.body
+  Book.findByIdAndUpdate(id, {title, description, author, rating})
+  .then((updatedBook) => res.redirect('/books'))
+  .catch(err => console.log(err))
+})
+
+
+// using the URL to read the ID and the body to read that data
+
+router.get('/edit/:id', (req, res)=>{
+  const id = req.params.id
+
+  // to read from the mongoose database can use:
+  Book.findById(id)
+  .then( bookFromfindById => res.render('book-edit', {book: bookFromfindById}))
+})
+
+router.get('/delete/:id', (req, res) => {
+  const id = req.params.id;
+  Book.findByIdAndDelete(id)
+  .then(deletedBook => res.redirect('/books'))
+})
+
+router.get('/:id', (req, res) => {
+  const id = req.params.id;
+  Book.findById(id)
+  .then( bookFromCollection => res.render("book-details" , bookFromCollection))
+});
+
+
+
 
 // ****************************************************************************************
 // GET route for displaying the book details page
 // ****************************************************************************************
 
-router.get('/books/:id', (req, res) => {
-  const id = req.params.id;
-  Book.findById(id)
+router.get('/', (req, res) => { // What URL does this answer?
+  Book.find()
+  .then(allBooks => {
+    res.render('books', {allBooks})})
    // You have to continue coding the route
 });
 
